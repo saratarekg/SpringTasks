40fcf7609e03f9d935ac9a8dd54812f627f69e6e
sara <76904492+saratarekg@users.noreply.github.com>
2024-08-19 10:47:48 +0300

    cleaned code

--- src/main/java/com/example/dependencyinjectiontask/CourseMapper.java
+++ src/main/java/com/example/dependencyinjectiontask/CourseMapper.java
@@ -3,13 +3,10 @@ package com.example.dependencyinjectiontask;
 import org.example.Course;
 import org.example.CourseDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+
 
 @Mapper(componentModel ="spring")
 public interface CourseMapper {
-    CourseMapper INSTANCE = Mappers.getMapper(CourseMapper.class);
     CourseDTO toCourseDTO(Course course);
     Course toCourse(CourseDTO courseDTO);
 }
