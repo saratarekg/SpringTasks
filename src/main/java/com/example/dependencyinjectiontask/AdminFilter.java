d70551ef59260321643d6b4745f957b476615707
sara <76904492+saratarekg@users.noreply.github.com>
2024-08-26 18:57:25 +0300

    admin filter added

---
+++ src/main/java/com/example/dependencyinjectiontask/AdminFilter.java
@@ -0,0 +1,44 @@
+package com.example.dependencyinjectiontask;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class AdminFilter extends HttpFilter {
+
+    @Override
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException, ServletException {
+        String uri = request.getRequestURI();
+
+        if (uri.startsWith("/swagger") || uri.startsWith("/v3/api-docs")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String token = request.getHeader("Authorization");
+
+        System.out.println("inside filter");
+
+        if (token==null ) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access denied");
+            return;
+        }
+
+        if (!"Admin".equals(token)) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access denied");
+            return;
+        }
+
+
+        filterChain.doFilter(request, response);
+
+    }
+
+}
